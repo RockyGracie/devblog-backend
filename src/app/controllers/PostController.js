3e0620708f8218e8e84ecd3e549775f821c19506
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-30 17:13:35 -0300

    Added title for posts

--- src/app/controllers/PostController.js
+++ src/app/controllers/PostController.js
@@ -19,34 +19,38 @@ class PostController {
   }
 
   async store(request, response) {
-    const { author, body } = request.body;
+    const { title, author, body } = request.body;
 
-    if (!author && !body) return response.status(400).json({ error: 'Post author and body are required.' });
+    if (!title && !author && !body) return response.status(400).json({ error: 'Post author and body are required.' });
+
+    if (!title) return response.status(400).json({ error: 'Post title is required.' });
 
     if (!author) return response.status(400).json({ error: 'Post author is required.' });
 
     if (!body) return response.status(400).json({ error: 'Post body is required.' });
 
-    const newPost = await PostRepository.create({ author, body });
+    const newPost = await PostRepository.create({ title, author, body });
 
     response.json(newPost);
   }
 
   async update(request, response) {
     const { id } = request.params;
-    const { author, body } = request.body;
+    const { title, author, body } = request.body;
 
     const postExists = await PostRepository.findById(id);
 
     if (!postExists) return response.status(400).json({ error: 'Post not found.' });
 
+    if (!title) return response.status(400).json({ error: 'Post\'s title is required.' });
+
     if (!author) return response.status(400).json({ error: 'Post\'s author is required.' });
 
     if (!body) return response.status(400).json({ error: 'Post\'s body is required.' });
 
-    if (!author && !body) return response.status(400).json({ error: 'Must update some value.' });
+    if (!title && !author && !body) return response.status(400).json({ error: 'Must update some value.' });
 
-    const updatedPost = await PostRepository.updateById(id, { author, body });
+    const updatedPost = await PostRepository.updateById(id, { title, author, body });
 
     response.json(updatedPost);
   }
--- src/app/repositories/PostRepository.js
+++ src/app/repositories/PostRepository.js
@@ -3,16 +3,19 @@ const { v4 } = require('uuid');
 let posts = [
   {
     id: v4(),
+    title: 'lalala',
     author: 'De Assis',
     body: 'Querida, ao pé do leito derradeiro...',
   },
   {
     id: v4(),
+    title: 'lalala2',
     author: 'Graça',
     body: 'Comovo-me em excesso, por natureza e por ofício. Acho medonho alguém viver sem paixões.',
   },
   {
     id: v4(),
+    title: 'lalala3',
     author: 'Joyce',
     body: 'Tudo é incerto neste mundo hediondo, mas não o amor de uma mãe.',
   },
@@ -27,10 +30,11 @@ class PostRepository {
     return new Promise((resolve) => resolve(posts.find((post) => post.id === id)));
   }
 
-  create({ author, body }) {
+  create({ title, author, body }) {
     return new Promise((resolve) => {
       const newPost = {
         id: v4(),
+        title: title[0].toUpperCase() + title.slice(1).toLowerCase(),
         author: author[0].toUpperCase() + author.slice(1).toLowerCase(),
         body: body[0].toUpperCase() + body.slice(1),
       };
@@ -40,10 +44,11 @@ class PostRepository {
     });
   }
 
-  updateById(id, { author, body }) {
+  updateById(id, { title, author, body }) {
     return new Promise((resolve) => {
       const updatedPost = {
         id,
+        title: title[0].toUpperCase() + title.slice(1).toLowerCase(),
         author: author ? author[0].toUpperCase() + author.slice(1).toLowerCase() : author,
         body: body ? body[0].toUpperCase() + body.slice(1) : body,
       };
