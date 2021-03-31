2e020d66accc7d13f8a018c6e04ab8f0228b6334
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-31 19:10:56 -0300

    Added Error Handler and express-async-errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
 
 app.listen(5000, () => console.log('Server started at localhost:5000'));
