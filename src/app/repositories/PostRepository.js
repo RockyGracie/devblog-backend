416277ee56fc3f756770eb7d9af77923577c25a7
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-31 15:49:47 -0300

    Created find all and find by id routes to database

--- src/app/repositories/PostRepository.js
+++ src/app/repositories/PostRepository.js
@@ -1,62 +1,28 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database/index');
 
-// let posts = [
-//   {
-//     id: v4(),
-//     title: 'Livros e Flores',
-//     author: 'De Assis',
-//     body: `Teus olhos são meus livros.
-//     Que livro há aí melhor,
-//     Em que melhor se leia
-//     A página do amor?
-
-//     Flores me são teus lábios.
-//     Onde há mais bela flor,
-//     Em que melhor se beba
-//     O bálsamo do amor?`,
-//   },
-//   {
-//     id: v4(),
-//     title: 'Frase Graciliano',
-//     author: 'Graça',
-//     body: `Quem escreve deve ter todo o cuidado para a coisa não sair molhada.
-//       Da página que foi escrita não deve pingar nenhuma palavra, a não ser as desnecessárias.
-//       É como pano lavado que se estira no varal.`,
-//   },
-//   {
-//     id: v4(),
-//     title: 'Frase James Joyce',
-//     author: 'Joyce',
-//     body: `Fizeste que eu confessasse os pavores que tenho.
-//       Mas vou te dizer também o que não me apavora.
-//       Não tenho medo de estar sozinho, de ser desdenhado por quem quer que seja,
-//       nem de deixar seja lá o que for que eu tenha que deixar.
-//       E não tenho medo, tampouco, de cometer um erro,
-//       um erro que dure toda a vida e talvez tanto quanto a própria eternidade mesma.`,
-//   },
-// ];
-
 class PostRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(posts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM posts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(posts.find((post) => post.id === id)));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM posts WHERE id = $1', [id]);
+
+    return row;
   }
 
   async create({
     title, author, body, category_id,
   }) {
-    const result = await db.query(`
+    const [row] = await db.query(`
       INSERT INTO posts(title, author, body, category_id)
       VALUES ($1, $2, $3, $4)
       RETURNING *
     `, [title, author, body, category_id]);
 
-    return result;
+    return row;
   }
 
   updateById(id, { title, author, body }) {
